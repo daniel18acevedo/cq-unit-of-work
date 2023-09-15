729eac9bdf747928684106c4b21b0c699f3c3cc5
Daniel Acevedo <daniel18acevedo@gmail.com>
2023-09-14 23:55:21 -0300

    New extension

---
+++ CQ.UnitOfWork.EfCore/Extensions/NoTrackingExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQ.UnitOfWork.EfCore.Extensions
+{
+    internal static class NoTrackingExtension
+    {
+        public static IQueryable<T> TrackElements<T>(this DbSet<T> elements, bool track) where T : class
+        {
+            return track ? elements : elements.AsNoTracking();
+        }
+    }
+}
