c65dacdacf330f06467e2c6aabd475afcdedc9bd
Daniel Acevedo <daniel18acevedo@gmail.com>
2023-09-29 14:48:46 -0300

    Merge abstractions with core

---
+++ CQ.UnitOfWork/Abstractions/IDatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQ.UnitOfWork.Abstractions
+{
+    public interface IDatabaseContext
+    {
+        bool Ping(string? collection = null);
+
+        Task SaveChangesAsync();
+
+        DatabaseInfo GetDatabaseInfo();
+    }
+}
---
+++ CQ.UnitOfWork/Abstractions/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿namespace CQ.UnitOfWork.Abstractions
+{
+    public interface IUnitOfWork
+    {
+        IRepository<TEntity> GetEntityRepository<TEntity>() where TEntity : class;
+
+        TRepository GetRepository<TRepository>() where TRepository : class;
+
+        Task CommitChangesAsync();
+    }
+}
